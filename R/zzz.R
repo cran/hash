a3ad372c3880b4db531ef4bcdffb9c23da512508
Christopher Brown <cbrown@opendatagroup.com>
2010-05-26 00:00:00 +0000

    version 2.0.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,23 @@
 
 .onLoad <- function( libname, pkgname ) {
 
-  if( is.null( getOption( "odg.logo" ) ) && ! getOption("verbose") ) {
-    width <- getOption("width")
-    options( width=80 )
-
-    cat(
-      "                            _       _        ",
-      "  ___  _ __   ___ _ __   __| | __ _| |_ __ _ ",
-      " / _ \\| '_ \\ / _ \\ '_ \\ / _` |/ _' | __/ _' |",
-      "| (_) | |_) |  __/ | | | (_| | (_| | || (_| |",
-      " \\___/| .__/ \\___|_| |_|\\__,_|\\__,_|\\__\\__,_|",
-      "      |_|       http://www.opendatagroup.com\n",
-      sep="\n"
-    )
-    options( width=width )
-    options( odg.logo = TRUE )
-
-  } 
+#  if( is.null( getOption( "odg.logo" ) ) && ! getOption("verbose") ) {
+#    width <- getOption("width")
+#    options( width=80 )
+#
+#    cat(
+#      "                            _       _        ",
+#      "  ___  _ __   ___ _ __   __| | __ _| |_ __ _ ",
+#      " / _ \\| '_ \\ / _ \\ '_ \\ / _` |/ _' | __/ _' |",
+#      "| (_) | |_) |  __/ | | | (_| | (_| | || (_| |",
+#      " \\___/| .__/ \\___|_| |_|\\__,_|\\__,_|\\__\\__,_|",
+#      "      |_|       http://www.opendatagroup.com\n",
+#      sep="\n"
+#    )
+#    options( width=width )
+#    options( odg.logo = TRUE )
+#
+#  } 
   
   cat( 
     "\n  ", 
