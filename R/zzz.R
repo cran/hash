6c71cbccf7f75b0b59866b96a7cf3f6f2a1ff731
Christopher Brown <chris.brown@decisionpatterns.com>
2012-05-15 00:00:00 +0000

    version 2.2.3

--- R/zzz.R
+++ R/zzz.R
@@ -2,11 +2,10 @@
 # .First.lib <- function( libname, pkgname ) {
 .onAttach <- function( libname, pkgname ) {
 
-  packageStartupMessage(
+  packageStartupMessage( 
     pkgname ,
     "-" ,
     utils::packageVersion(pkgname, libname),
-#    utils::installed.packages()[ pkgname , "Version"],
     " provided by Decision Patterns\n" ,
     domain = NA
   )
